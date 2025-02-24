8c47334db0053ec9eb6054b593101aa42e00cd6a
YangKlee <147032564+YangKlee@users.noreply.github.com>
2025-02-24 18:56:33 +0700

    upppp

---
+++ Ex_Basic_1/MaPhuongLe/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaPhuongLe
+{
+    internal class Program
+    {
+        static void inilazie(int[,] a, int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                a[i, i] = 0;
+            }
+        }
+        static void show(int[,]a, int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            n = Int16.Parse(Console.ReadLine());
+            int[,] a = new int[101, 101];
+            inilazie(a, n);
+            int count = 1;
+            a[1, (n / 2) + 1] = 1;
+            int i = 1, j = n/2;
+
+            while (count <= n*n)
+            {
+                i--; j++;
+                
+                count++;
+                if (i < 1 && j >=1)
+                {
+                    
+                    i = n;
+                    j++;
+                    a[i, j] = count;
+                    continue;
+                }
+                else if (j > n && i < n)
+                {
+                    i--;
+                    j = 1;
+                    a[i, j] = count;
+                    continue;
+                }
+
+            }
+            show(a, n);
+
+        }
+    }
+}
